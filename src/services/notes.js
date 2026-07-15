5d38176dd2d326972ca13091b2b72a1d99c31e69
Saif Shalan <safensound95@gmail.com>
2026-07-15 11:01:35 +0300

    reafactor: change baseUrl to be relative and dynamic

--- src/services/notes.js
+++ src/services/notes.js
@@ -1,26 +1,22 @@
 import axios from "axios";
 
-const axiosInstance = axios.create({
-  baseURL: "http://localhost:3001",
-});
-
 const getNotes = async () => {
-  const response = await axiosInstance.get("/api/notes");
+  const response = await axios.get("/api/notes");
   return response.data;
 };
 
 const addNote = async (newNote) => {
-  const response = await axiosInstance.post("/api/notes", newNote);
+  const response = await axios.post("/api/notes", newNote);
   return response.data;
 };
 
 const updateNote = async (id, updatedNote) => {
-  const response = await axiosInstance.put(`/api/notes/${id}`, updatedNote);
+  const response = await axios.put(`/api/notes/${id}`, updatedNote);
   return response.data;
 };
 
 const deleteNote = async (id) => {
-  await axiosInstance.delete(`/api/notes/${id}`);
+  await axios.delete(`/api/notes/${id}`);
 };
 
 export default { getNotes, addNote, updateNote, deleteNote };
